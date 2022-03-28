68fea4701edcbabea4d1c2e8e331da9a6abaebb1
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2022-03-27 17:34:31 -0700

    Moved project data to json & api calls to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,101 @@ import "./App.css";
 import Projects from "./Projects.jsx";
 import Bio from "./Bio.jsx";
 import Contact from "./Contact.jsx";
+import projects from "./projects.json";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <Bio />
-                {/* TODO: Demos */}
-                <Projects />
-                <Contact />
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects
+    };
+  }
+  componentDidMount() {
+    // Fetch download counts
+    for (let project of this.state.projects) {
+      if (!project.distributions) continue;
+      for (let distribution of Object.values(project.distributions)) {
+        switch (distribution.platform) {
+          case "tml":
+            this.fetchTmlData(distribution);
+            break;
+          case "steam":
+            this.fetchSteamData(distribution);
+            break;
+          case "curseforge":
+            this.fetchCurseforgeData(distribution);
+            break;
+          case "modrinth":
+            this.fetchModrinthData(distribution);
+            break;
+
+          default:
+            console.warn("Unsupported distribution platform:", distribution.platform);
+            break;
+        }
+      }
     }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Bio />
+        {/* TODO: Demos */}
+        <Projects projects={projects} />
+        <Contact />
+      </div>
+    );
+  }
+
+  async fetchSteamData(distribution) {
+    // Steam API doesn't allow requests from clients so we need to make the server do it
+    await fetch(`/api/steam?id=${distribution.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        distribution.downloads = data.response.publishedfiledetails[0].lifetime_subscriptions;
+      })
+      .catch((error) => {
+        distribution.downloads = error.toString();
+      });
+    this.forceUpdate();
+  }
+
+  async fetchCurseforgeData(distribution) {
+    await fetch(`https://api.cfwidget.com/minecraft/mods/${distribution.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        distribution.downloads = data.downloads.total;
+      })
+      .catch((error) => {
+        distribution.downloads = error.toString();
+      });
+    this.forceUpdate();
+  }
+
+  async fetchModrinthData(distribution) {
+    await fetch(`https://api.modrinth.com/api/v1/mod/${distribution.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        distribution.downloads = data.downloads;
+      })
+      .catch((error) => {
+        distribution.downloads = error.toString();
+      });
+    this.forceUpdate();
+  }
+
+  async fetchTmlData(distribution) {
+    await fetch(`/api/tml?id=${distribution.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        distribution.downloads = data.downloads;
+      })
+      .catch((error) => {
+        distribution.downloads = error.toString();
+      });
+    this.forceUpdate();
+  }
 }
 
 export default App;
--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -3,137 +3,88 @@ import "./Card.css";
 import Tag from "./Tag.jsx";
 
 class Card extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            downloadsSteam: "loading...",
-            downloadsCurse: "loading...",
-            downloadsModrinth: "loading...",
-            downloadsTML: "loading..."
-        };
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={`card${this.shouldShowCard() ? "" : " display-none"}`}>
+        <img src={"./images/" + this.props.icon} alt="card icon" />
+        <h3>{this.props.title}</h3>
+        <p>{this.props.description}</p>
+        {this.getDownloadCount()}
+        <div className="links">
+          {this.getLink()}
+          {this.getGitHubLink()}
+        </div>
+        {this.getTags()}
+      </div>
+    );
+  }
+
+  shouldShowCard() {
+    if (Object.values(this.props.allTagStates).every((i) => i === Object.values(this.props.allTagStates)[0])) {
+      // All tags are equal, so we shouldn't be filtering
+      return true;
     }
-
-    componentDidMount() {
-        // Fetch download count then update state
-        if (this.props.idSteam) {
-            // Steam API doesn't allow requests from clients so we need to make the server do it
-            fetch(`/api/steam?id=${this.props.idSteam}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    this.setState({ downloadsSteam: data.response.publishedfiledetails[0].lifetime_subscriptions });
-                })
-                .catch((error) => {
-                    this.setState({ downloadsSteam: error.toString() });
-                });
-        }
-
-        if (this.props.idCurse && this.props.idModrinth) {
-            fetch(`https://api.cfwidget.com/minecraft/mods/${this.props.idCurse}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    this.setState({ downloadsCurse: data.downloads.total });
-                })
-                .catch((error) => {
-                    this.setState({ downloadsCurse: error.toString() });
-                });
-            fetch(`https://api.modrinth.com/api/v1/mod/${this.props.idModrinth}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    this.setState({ downloadsModrinth: data.downloads });
-                })
-                .catch((error) => {
-                    this.setState({ downloadsModrinth: error.toString() });
-                });
-        }
-
-        if (this.props.idTML) {
-            fetch(`/api/tml?id=${this.props.idTML}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    this.setState({ downloadsTML: data.downloads });
-                })
-                .catch((error) => {
-                    this.setState({ downloadsTML: error.toString() });
-                });
-        }
+    for (let tag of this.props.tags) {
+      if (this.props.allTagStates[tag]) {
+        // At least one tag on this card is selected
+        return true;
+      }
     }
+  }
 
-    render() {
-        return (
-            <div className={`card${this.shouldShowCard() ? "" : " display-none"}`}>
-                <img src={this.props.icon} alt="card icon" />
-                <h3>{this.props.title}</h3>
-                <p>{this.props.description}</p>
-                {this.getDownloadCount()}
-                <div className="links">
-                    {this.getLink()}
-                    {this.getGitHubLink()}
-                </div>
-                {this.getTags()}
-            </div>
-        );
+  getDownloadCount() {
+    if (!this.props.distributions) return;
+    if (this.props.distributions.tml) {
+      return (
+        <p>
+          Downloads ({this.linkIfExists(this.props.distributions.tml.link, "TModLoader")}): <b>{this.props.distributions.tml.downloads || "Loading..."}</b>
+        </p>
+      );
     }
+    if (this.props.distributions.steam) {
+      return (
+        <p>
+          Downloads ({this.linkIfExists(this.props.distributions.steam.link, "Steam")}): <b>{this.props.distributions.steam.downloads || "Loading..."}</b>
+        </p>
+      );
+    }
+    if (this.props.distributions.curseforge || this.props.distributions.modrinth) {
+      return (
+        <p>
+          Downloads ({this.linkIfExists(this.props.distributions.curseforge.link, "Curse")},{" "}
+          {this.linkIfExists(this.props.distributions.modrinth.link, "Modrinth")}):{" "}
+          <b>{this.props.distributions.curseforge.downloads + this.props.distributions.modrinth.downloads || "Loading..."}</b>
+        </p>
+      );
+    }
+  }
 
-    shouldShowCard() {
-        if (Object.values(this.props.allTagStates).every(i => i === Object.values(this.props.allTagStates)[0])) {
-            // All tags are equal, so we shouldn't be filtering
-            return true;
-        }
-        for (let tag of this.props.tags) {
-            if (this.props.allTagStates[tag]) {
-                // At least one tag on this card is selected
-                return true;
-            }
-        }
-    }
+  linkIfExists(link, text) {
+    return link ? <a href={link}>{text}</a> : text;
+  }
 
-    getDownloadCount() {
-        if (this.props.idTML) {
-            return (
-                <p>
-                    Downloads ({this.linkIfExists(this.props.linkTML, "TModLoader")}): <b>{this.state.downloadsTML}</b>
-                </p>
-            );
-        }
-        if (this.props.idSteam) {
-            return (
-                <p>
-                    Downloads ({this.linkIfExists(this.props.linkSteam, "Steam")}): <b>{this.state.downloadsSteam}</b>
-                </p>
-            );
-        }
-        if (this.props.idCurse && this.props.idModrinth) {
-            return (
-                <p>
-                    Downloads ({this.linkIfExists(this.props.linkCurse, "Curse")}, {this.linkIfExists(this.props.linkModrinth, "Modrinth")}):{" "}
-                    <b>{this.state.downloadsCurse + this.state.downloadsModrinth}</b>
-                </p>
-            );
-        }
-    }
+  getLink() {
+    return this.props.link && <a href={this.props.link}>Webpage</a>;
+  }
 
-    linkIfExists(link, text) {
-        return link ? <a href={link}>{text}</a> : text;
-    }
+  getGitHubLink() {
+    return this.props.github && <a href={"https://github.com/AlurienFlame/" + this.props.github}>GitHub</a>;
+  }
 
-    getLink() {
-        return this.props.link && <a href={this.props.link}>Webpage</a>;
-    }
-
-    getGitHubLink() {
-        return this.props.github && <a href={this.props.github}>GitHub</a>;
-    }
-
-    getTags() {
-        return (
-            <div className="tags">
-                {this.props.tags &&
-                    this.props.tags.map((tagID) => {
-                        return <Tag key={tagID} tagID={tagID} selected={this.props.allTagStates[tagID]} toggleTag={this.props.toggleTag} />;
-                    })}
-            </div>
-        );
-    }
+  getTags() {
+    return (
+      <div className="tags">
+        {this.props.tags &&
+          this.props.tags.map((tagID) => {
+            return <Tag key={tagID} tagID={tagID} selected={this.props.allTagStates[tagID]} toggleTag={this.props.toggleTag} />;
+          })}
+      </div>
+    );
+  }
 }
 
 export default Card;
