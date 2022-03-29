2a322f5b98d2f8395180e2fcc01a9d9575b71d3e
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2022-03-28 21:02:03 -0700

    Added "More Info" buttons (incomplete)

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -4,12 +4,11 @@ import Tag from "./Tag.jsx";
 
 class Card extends React.Component {
   render() {
-    // TODO: Cards with distributors should have a "website" button that brings up a popup listing distributors
     return (
       <div className={`card${this.shouldShowCard() ? "" : " display-none"}`}>
         <img src={"./images/" + this.props.icon} alt="card icon" />
         <h3>{this.props.title}</h3>
-        <p>{this.props.description}</p>
+        <p className="description">{this.props.description}</p>
         {this.getDownloadCount()}
         <div className="links">
           {this.props.github && (
@@ -19,9 +18,9 @@ class Card extends React.Component {
                 window.open("https://github.com/AlurienFlame/" + this.props.github);
               }}
             >
-              <svg class="logo">
+              <svg className="logo">
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
                 />
               </svg>
@@ -35,10 +34,29 @@ class Card extends React.Component {
                 window.open(this.props.link);
               }}
             >
-              <svg class="logo" viewBox="0 0 460.114 460.114">
+              <svg className="logo" viewBox="0 0 460.114 460.114">
                 <path d="M393.538 203.629L102.557 5.543a31.97 31.97 0 0 0-32.94-1.832 31.97 31.97 0 0 0-17.022 28.26v396.173a31.97 31.97 0 0 0 17.022 28.26c10.471 5.539 23.147 4.834 32.94-1.832l290.981-198.087a31.97 31.97 0 0 0 13.98-26.428c.001-10.58-5.233-20.475-13.98-26.428z" />
               </svg>
               <span>Demo</span>
+            </button>
+          )}
+          {this.props.distributions && (
+            <button
+              type="button"
+              onClick={() => {
+                window.open(this.props.distributions[Object.keys(this.props.distributions)[0]].link);
+                if (Object.keys(this.props.distributions).length > 1) {
+                  // TODO: popup for multiple distributors
+                  console.log("TODO: Multiple distributors");
+                } else {
+                  console.log("Open single window");
+                }
+              }}
+            >
+              <svg className="logo" viewBox="0 0 460 460">
+                <path d="M230 0C102.975 0 0 102.975 0 230s102.975 230 230 230 230-102.974 230-230S357.025 0 230 0zm38.333 377.36c0 8.676-7.034 15.71-15.71 15.71h-43.101c-8.676 0-15.71-7.034-15.71-15.71V202.477c0-8.676 7.033-15.71 15.71-15.71h43.101c8.676 0 15.71 7.033 15.71 15.71V377.36zM230 157c-21.539 0-39-17.461-39-39s17.461-39 39-39 39 17.461 39 39-17.461 39-39 39z" />
+              </svg>
+              <span>More Info</span>
             </button>
           )}
         </div>
@@ -65,21 +83,21 @@ class Card extends React.Component {
     if (!this.props.distributions) return;
     if (this.props.distributions.tml) {
       return (
-        <p>
+        <p className="downloads">
           Downloads: <b>{this.props.distributions.tml.downloads || "Loading..."}</b>
         </p>
       );
     }
     if (this.props.distributions.steam) {
       return (
-        <p>
+        <p className="downloads">
           Downloads: <b>{this.props.distributions.steam.downloads || "Loading..."}</b>
         </p>
       );
     }
     if (this.props.distributions.curseforge || this.props.distributions.modrinth) {
       return (
-        <p>
+        <p className="downloads">
           Downloads: <b>{this.props.distributions.curseforge.downloads + this.props.distributions.modrinth.downloads || "Loading..."}</b>
         </p>
       );
